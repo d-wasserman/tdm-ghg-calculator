55340cfec3b8b73d2f9a6381255c1cd62c37d820
David Wasserman <david.wasserman.plan@gmail.com>
2026-03-31 12:25:53 -0700

    Add first tests
    Workflows and first passing test.

---
+++ tests/test_tdm_calcs.py
@@ -0,0 +1,30 @@
+"""Unit tests for tdm-calcs library."""
+from unittest.mock import patch, MagicMock
+
+import numpy as np
+import pytest
+from math import isclose
+import tdm_ghg 
+
+# ---- Constants ----
+
+class TestMitigations:
+    def test_t1_increase_residential_density_strategy(self):
+        """Test the residential density strategy."""
+        # Mock the input data
+        proposed_residential_density = 16
+        typical_residential_density = 9.1
+        elasticity_vmt_residential_density = -0.22        
+        # Expected result based on the mock data
+        expected_result = -0.166
+        
+        # Call the function under test
+        result = tdm_ghg.mitigations.t1_increase_residential_density(proposed_residential_density, typical_residential_density, elasticity_vmt_residential_density)
+        
+        # Assert that the result matches the expected output
+        assert isclose(result, expected_result, rel_tol=1e-2), \
+            f"Expected {expected_result}, but got {result}"
+            
+        
+
+
