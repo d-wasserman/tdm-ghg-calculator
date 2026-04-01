a3cdeaca687c9a5bba2437e95b89f3a585b2eb6a
David Wasserman <david.wasserman.plan@gmail.com>
2026-03-31 23:00:35 -0700

    Update test_tdm_calcs.py
    Fix text parameter input order. Ruff formatting.

--- tests/test_tdm_calcs.py
+++ tests/test_tdm_calcs.py
@@ -1,10 +1,12 @@
 """Unit tests for tdm-calcs library."""
+
 import numpy as np
 import pytest
 from math import isclose
-import tdm_ghg 
+import tdm_ghg
 
 # ---- Constants ----
+
 
 class TestMitigations:
     def test_t1_increase_residential_density_strategy(self):
@@ -12,46 +14,56 @@ class TestMitigations:
         # Mock the input data
         proposed_residential_density = 16
         typical_residential_density = 9.1
-        elasticity_vmt_residential_density = -0.22        
+        elasticity_vmt_residential_density = -0.22
         # Expected result based on the mock data
         expected_result = -0.166
-        
+
         # Call the function under test
-        result = tdm_ghg.mitigations.t1_increase_residential_density(proposed_residential_density, typical_residential_density, elasticity_vmt_residential_density)
-        
+        result = tdm_ghg.mitigations.t1_increase_residential_density(
+            proposed_residential_density,
+            typical_residential_density,
+            elasticity_vmt_residential_density,
+        )
+
         # Assert that the result matches the expected output
-        assert isclose(result, expected_result, rel_tol=1e-2), \
+        assert isclose(result, expected_result, rel_tol=1e-2), (
             f"Expected {expected_result}, but got {result}"
-            
+        )
+
     def test_t2_increase_job_density_strategy(self):
         """Test the job density strategy."""
         # Mock the input data
         proposed_job_density = 500
         typical_job_density = 145
-        elasticity_vmt_job_density = -0.07        
+        elasticity_vmt_job_density = -0.07
         # Expected result based on the mock data
         expected_result = -0.171
-        
+
         # Call the function under test
-        result = tdm_ghg.mitigations.t2_increase_job_density(proposed_job_density, typical_job_density, elasticity_vmt_job_density)
-        
+        result = tdm_ghg.mitigations.t2_increase_job_density(
+            proposed_job_density, typical_job_density, elasticity_vmt_job_density
+        )
+
         # Assert that the result matches the expected output
-        assert isclose(result, expected_result, rel_tol=1e-2), \
+        assert isclose(result, expected_result, rel_tol=1e-2), (
             f"Expected {expected_result}, but got {result}"
+        )
+
     def test_t3_provide_tod_strategy(self):
         """Test the tod strategy."""
         # Mock the input data
         transit_mode_share = 4
+        auto_mode_share = -90
         ratio = 4.9
-        auto_mode_share = -90        
         # Expected result based on the mock data
         expected_result = -0.272
-        
+
         # Call the function under test
-        result = tdm_ghg.mitigations.t3_provide_transit_oriented_development(transit_mode_share,ratio,auto_mode_share)
-        
+        result = tdm_ghg.mitigations.t3_provide_transit_oriented_development(
+            transit_mode_share, auto_mode_share, ratio
+        )
+
         # Assert that the result matches the expected output
-        assert isclose(result, expected_result, rel_tol=1e-2), \
+        assert isclose(result, expected_result, rel_tol=1e-2), (
             f"Expected {expected_result}, but got {result}"
-
-
+        )
